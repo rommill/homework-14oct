ed67f351609abfc7a246814ef2184caffeada700
Rommill <rommill19@gmail.com>
2025-10-14 23:52:26 +0300

    feat: Completed all styling and logic for Home Task #1

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,49 +1,55 @@
-import React from 'react'
-import s from './FriendMessage.module.css'
+import React from "react";
+import s from "./FriendMessage.module.css";
+import { MessageType } from "../HW1";
 
-// создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
-    return (
-        <div
-            id={'hw1-friend-message-' + props.message.id}
-            className={s.friendMessage}
-        >
-            <div className={s.friendImageAndText}>
-                <img
-                    id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
+export type FriendMessagePropsType = {
+  message: MessageType;
+};
 
-                    //
-                />
-                <div className={s.friendText}>
-                    <div
-                        id={'hw1-friend-name-' + props.message.id}
-                        className={s.friendName}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre
-                        id={'hw1-friend-text-' + props.message.id}
-                        className={s.friendMessageText}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </pre>
-                </div>
-            </div>
-            <div
-                id={'hw1-friend-time-' + props.message.id}
-                className={s.friendTime}
-            >
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
+// Создать тип вместо any и отобразить приходящие данные
+const FriendMessage = (props: FriendMessagePropsType) => {
+  return (
+    <div
+      id={"hw1-friend-message-" + props.message.id}
+      className={s.friendMessage}
+    >
+      <div className={s.friendImageAndText}>
+        {/* НОВЫЙ КОНТЕЙНЕР: Оборачиваем аватар и время */}
+        <div className={s.avatarAndTimeContainer}>
+          <img
+            id={"hw1-friend-avatar-" + props.message.id}
+            src={props.message.user.avatar}
+            alt={props.message.user.name + " avatar"}
+            className={s.friendAvatar}
+          />
+          <div
+            id={"hw1-friend-time-" + props.message.id}
+            className={s.friendTime}
+          >
+            {/* Время перенесено сюда */}
+            {props.message.message.time}
+          </div>
         </div>
-    )
-}
+        {/* КОНЕЦ НОВОГО КОНТЕЙНЕРА */}
 
-export default FriendMessage
+        <div className={s.friendText}>
+          <div
+            id={"hw1-friend-name-" + props.message.id}
+            className={s.friendName}
+          >
+            {props.message.user.name}
+          </div>
+          <pre
+            id={"hw1-friend-text-" + props.message.id}
+            className={s.friendMessageText}
+          >
+            {props.message.message.text}
+          </pre>
+        </div>
+      </div>
+      {/* СТАРОЕ РАСПОЛОЖЕНИЕ ВРЕМЕНИ УДАЛЕНО */}
+    </div>
+  );
+};
+
+export default FriendMessage;
